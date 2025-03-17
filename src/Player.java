d13d87a73fa48e9575f8db57ba9242d5c1795daf
michellel314 <michelle.lee.087@gmail.com>
2025-03-17 14:00:36 -0400

    Second Commit - Planning Structure

--- src/Player.java
+++ src/Player.java
@@ -1,3 +1,27 @@
-public class Player {
 
+public class Player {
+    private int score;
+    private String name;
+
+    public Player(String name, int score) {
+        this.name = name;
+        score = 0;
+
+    }
+
+    public int getScore(){
+        return score;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void addScore(int newPoints){
+        score += newPoints;
+    }
+
+    public void setScore(int newScore){
+        score = newScore;
+    }
 }
---
+++ src/Timer.java
@@ -0,0 +1,30 @@
+import java.util.TimerTask;
+
+public class Timer {
+        Timer timer = new Timer();
+
+        public Timer()
+        TimerTask task1 = new TimerTask() {
+            public void run() {
+                System.out.println("Task 1 executed!");
+            }
+        };
+
+        TimerTask task2 = new TimerTask() {
+            public void run() {
+                System.out.println("Task 2 executed!");
+            }
+        };
+
+        TimerTask task3 = new TimerTask() {
+            public void run() {
+                System.out.println("Task 3 executed!");
+            }
+        };
+
+
+        timer.schedule(task1, 2000);
+        timer.schedule(task2, 4000);
+        timer.schedule(task3, 6000);
+
+}
