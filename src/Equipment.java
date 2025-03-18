7d7ac06770b805b7fbddb400a303f780e5ea6c80
BT_1N3_31 <jintaoh123@gmail.com>
2025-03-18 14:02:31 -0400

    commiting commit of commiting

--- src/Equipment.java
+++ src/Equipment.java
@@ -1,2 +1,26 @@
 public class Equipment{
+
+    private int uses;
+    String name;
+    int score;
+
+    public Equipment(String name, int score, int uses) {
+        this.uses = uses;
+        this.name = name;
+        this.score = score;
+    }
+
+    public int getUses() {
+        return uses;
+    }
+
+    public String equipmentUsed() {
+        if (uses == 0) {
+            return name + " is broken.";
+        } else {
+            uses--;
+            return "you used " + name;
+        }
+    }
+
 }
