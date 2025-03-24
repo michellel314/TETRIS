52aad1abe65ea96406beb5ee1da1f49e97a8fee4
michellel314 <michelle.lee.087@gmail.com>
2025-03-24 13:57:37 -0400

    Tenth Commit - Adding music for game

--- src/Music.java
+++ src/Music.java
@@ -1,2 +1,24 @@
+import javax.sound.sampled.*;
+import java.io.IOException;
+import java.util.Objects;
+
 public class Music {
+    private Clip clip;
+    public Music(String audioFilePath) throws LineUnavailableException, IOException, UnsupportedAudioFileException {
+        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream(audioFilePath)));
+        clip = AudioSystem.getClip();
+        clip.open(audioInputStream);
+    }
+
+    public void play() {
+        clip.start();
+    }
+
+    public void stop() {
+        clip.stop();
+    }
+
+    public void close() {
+        clip.close();
+    }
 }
